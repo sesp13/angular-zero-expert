b4199b8ec0977985fe3257fcf4ef8f573d87f8d3
sesp13 <sespinosarte99@gmail.com>
2022-03-09 01:20:26 -0500

    05-heroesApp: rename main project and create basic components

--- 05-heroesApp/src/app/app.module.ts
+++ 05-heroesApp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
