37b685b2bc5d7dd01d2c1adc13d9d40f30e4c14a
sesp13 <sespinosarte99@gmail.com>
2022-03-24 10:46:32 -0500

    09-mapsApp: create navigation menu

--- 09-maps/src/app/shared/menu/menu.component.ts
+++ 09-maps/src/app/shared/menu/menu.component.ts
@@ -1,16 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { MenuItem } from 'src/app/interfaces/menu-item.interface';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class MenuComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class MenuComponent {
+  menuItems: MenuItem[] = [
+    {
+      route: 'maps/full-screen',
+      name: 'Full Screen',
+    },
+    {
+      route: 'maps/zoom-range',
+      name: 'Zoom Range',
+    },
+    {
+      route: 'maps/markers',
+      name: 'Markers',
+    },
+    {
+      route: 'maps/properties',
+      name: 'Properties',
+    },
+  ];
 }
