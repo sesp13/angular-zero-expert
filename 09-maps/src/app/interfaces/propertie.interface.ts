66b5de251e10d5ae35cecd050bf8ee972fb833c1
sesp13 <sespinosarte99@gmail.com>
2022-03-26 23:01:32 -0500

    09: mapsApp add properties section

---
+++ 09-maps/src/app/interfaces/propertie.interface.ts
@@ -0,0 +1,5 @@
+export interface Propertie {
+  title: string;
+  description: string;
+  lngLat: [number, number];
+}
--- 09-maps/src/app/maps/components/mini-map/mini-map.component.ts
+++ 09-maps/src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,16 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  ViewChild,
+} from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-mini-map',
   templateUrl: './mini-map.component.html',
   styles: [
-  ]
+    `
+      .map {
+        width: 100%;
+        height: 150px;
+        margin: 0px;
+      }
+    `,
+  ],
 })
-export class MiniMapComponent implements OnInit {
+export class MiniMapComponent implements AfterViewInit {
+  @Input() lngLat: [number, number] = [0, 0];
+  @ViewChild('map') mapDiv!: ElementRef;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    const map = new mapboxgl.Map({
+      container: this.mapDiv.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.lngLat,
+      zoom: 15,
+      // interactive: false
+    });
+    new mapboxgl.Marker({draggable: true}).setLngLat(this.lngLat).addTo(map);
   }
-
 }
