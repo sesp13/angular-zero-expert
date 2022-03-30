808ee885977d83e8d6bf6c3c9a55f0f722bb38ee
sesp13 <sespinosarte99@gmail.com>
2022-03-30 14:51:58 -0500

    12-AuthApp: add production env and specify hash urls config

--- 12-authApp/src/app/app-routing.module.ts
+++ 12-authApp/src/app/app-routing.module.ts
@@ -23,7 +23,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    // useHash: true
+    useHash: false
+  })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- 12-authApp/src/environments/environment.prod.ts
+++ 12-authApp/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://localhost:3000/api'
+  baseUrl: 'https://auth-mean-sesp-13.herokuapp.com/api'
 };
